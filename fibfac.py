ad2985d6cda5583c4e08287d1fb7ee9aaf9bf3ed
Pete Vilter <pete.vilter@gmail.com>
2010-12-14 23:05:00 -0500

    monospace font; show spinner while working

--- fibfac.py
+++ fibfac.py
@@ -4,5 +4,11 @@ def fac(n):
     else:
         return n * fac(n - 1)
 
+def fib(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
 if __name__ == '__main__':
     print fac(200)
